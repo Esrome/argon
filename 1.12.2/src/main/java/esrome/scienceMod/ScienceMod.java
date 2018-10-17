64809b628674ab7f846b358922d8ca357d1e4095
Esrome <esromethestrange@gmail.com>
2018-10-16 22:39:53 -0600

    Initial commit

---
+++ 1.12.2/src/main/java/esrome/scienceMod/ScienceMod.java
@@ -0,0 +1,37 @@
+package esrome.scienceMod;
+
+import esrome.scienceMod.proxy.ServerProxy;
+import esrome.scienceMod.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class ScienceMod {
+
+	@Instance
+	public static ScienceMod instance;
+	
+	@SidedProxy(clientSide = Reference.CLIENT_PROXY, serverSide = Reference.SERVER_PROXY)
+	public static ServerProxy proxy;
+	
+	@EventHandler
+	public static void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void postInit(FMLPostInitializationEvent event) {
+		
+	}
+	
+}
---
+++ 1.12.2/src/main/java/esrome/scienceMod/util/Reference.java
@@ -0,0 +1,13 @@
+package esrome.scienceMod.util;
+
+public class Reference {
+
+	public static final String MODID = "esromeScienceMod";
+	public static final String NAME = "Esrome's Science Mod";
+	public static final String VERSION = "1.0.0";
+	public static final String COMPATIBLE_VERSIONS = "[1.12.2]";
+	
+	public static final String CLIENT_PROXY = "esrome.scienceMod.proxy.ClientProxy";
+	public static final String SERVER_PROXY = "esrome.scienceMod.proxy.ServerProxy";
+	
+}
